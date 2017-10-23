2d5629372db16cb368eeb34714e8ec138c81b93a
Locker <LockerWarUA@gmail.com>
2017-10-23 22:06:22 +0200

    First commit

---
+++ CW_6.java
@@ -0,0 +1,37 @@
+package CW_V6;
+
+public class CW_6 {
+        public static void main(String[] args) {
+	  int x = (int)(Math.random()*10);
+	  int y = (int)(Math.random()*10);
+	  int z = (int)(Math.random()*10);
+	  if (x > z && x > y && x != z && x != y){
+          System.out.println("--------------------------");
+          System.out.println(x);
+          System.out.println(y);
+          System.out.println(z);
+          System.out.println("--------------------------");
+	      System.out.println("x!");
+      }
+      if (y > z && y > x && y != z && y != x){
+          System.out.println("--------------------------");
+          System.out.println(x);
+          System.out.println(y);
+          System.out.println(z);
+          System.out.println("--------------------------");
+          System.out.println("y!");
+      }
+      if (z > x && z > y && z != y && z != x){
+          System.out.println("--------------------------");
+          System.out.println(x);
+          System.out.println(y);
+          System.out.println(z);
+          System.out.println("--------------------------");
+          System.out.println("z!");
+      }
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+                }
+    }
+
