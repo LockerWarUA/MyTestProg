f6ae78783048bba8926fade3ba1bd89ad01fa0f2
Locker <LockerWarUA@gmail.com>
2017-10-23 23:31:39 +0200

    Second commit

--- CW_4.java
+++ CW_4.java
@@ -1,7 +1,11 @@
 package CW_V6;
-
+import java.util.Scanner;
 public class CW_4 {
     public static void main(String[] args) {
-
+    char znak;
+    Scanner sc = new Scanner(System.in);
+    znak = sc.next().charAt(0);
+    int bot = (int)(znak);
+    System.out.println(bot);
     }
 }
